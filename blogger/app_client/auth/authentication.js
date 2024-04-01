a5f48e207c07cd0be2fd1e7c50e4dbbe57444ba1
kuhaku4 <edalmonr@millersville.edu>
2024-04-01 18:21:22 -0400

    Lab6

--- blogger/app_client/auth/authentication.js
+++ blogger/app_client/auth/authentication.js
@@ -15,29 +15,20 @@ app.service('authentication', authentication);
           return token;
       };
 
-      var register = function(user) {
-        console.log('Registering user ' + user.email + ' ' + user.password);
-        return $http.post('/api/register', user).then(function(data){
-          saveToken(data.data.token);
-          console.log('Registration successful');
-          return data;
-        }, function(err){
-          console.log('Error registering:', err);
-          throw err;
-        });
-      };
+        var register = function(user) {
+            console.log('Registering user ' + user.email + ' ' + user.password);
+            return $http.post('/api/register', user).then(function(data){
+              saveToken(data.data.token);
+          });
+        };
 
-      var login = function(user) {
-        console.log('Attempting to login user ' + user.email + ' ' + user.password);
-        return $http.post('/api/login', user).then(function(data) {
-          saveToken(data.data.token);
-          console.log('Login successful');
-          return data;
-        }, function(err){
-          console.log('Error logging in:', err);
-          throw err;
-        });
-      };
+        var login = function(user) {
+           console.log('Attempting to login user ' + user.email + ' ' + user.password);
+            return $http.post('/api/login', user).then(function(data) {
+
+              saveToken(data.data.token);
+           });
+        };
 
         var logout = function() {
             console.log("Logging out");
@@ -93,28 +84,30 @@ app.controller('LoginController', ['$location', 'authentication', function Login
     vm.returnPage = $location.search().page || '/';
 
     vm.onSubmit = function () {
-        console.log('Form submitted');
-        vm.formError = "";
-        if (!vm.credentials.email || !vm.credentials.password) {
-          vm.formError = "All fields required, please try again";
-          return false;
-        } else {
-          vm.formError = ""; // Reset the formError property
-          vm.doLogin();
-        }
-      };
+      vm.formError = "";
+      if (!vm.credentials.email || !vm.credentials.password) {
+           vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+           vm.doLogin();
+      }
+    };
 
-      vm.doLogin = function() {
-        vm.formError = "";
-        authentication
-          .login(vm.credentials)
-          .then(function(){
-            $location.path('blogs');
-          }
-          , (function(err){
-            vm.formError = "Login failed. Email or password maybe incorrect.";
-          }));
-      };
+    vm.doLogin = function() {
+      console.log("DoLogin Ran");
+      vm.formError = "";
+      authentication
+        .login(vm.credentials)
+        .then(function(){
+          $location.path('blogList');
+        }
+        , (function(err){
+          var obj = err;
+          console.log(obj.message);
+          //vm.formError = obj.message;
+          vm.formError = "Login failed. Email or password maybe incorrect.";
+        }));
+    };
  }]);
 
 app.controller('RegisterController', ['$location', 'authentication', function RegisterController($location, authentication) {
@@ -131,28 +124,26 @@ app.controller('RegisterController', ['$location', 'authentication', function Re
     };
 
     vm.onSubmit = function () {
-        console.log('Form submitted');
-        vm.formError = "";
-        if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
-          vm.formError = "All fields required, please try again";
-          return false;
-        } else {
-          vm.formError = ""; // Reset the formError property
-          vm.doRegister();
-        }
-      };
+      vm.formError = "";
+      if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+        vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+        vm.doRegister();
+      }
+    };
 
-      vm.doRegister = function() {
-        vm.formError = "";
-        authentication
-          .register(vm.credentials)
-          .then(function(){
-            $location.path('blogs');
-          }
-          , (function(err){
-            vm.formError = "Error registering. Try again with a different email address."
-          }));
-      };
+    vm.doRegister = function() {
+      vm.formError = "";
+      authentication
+        .register(vm.credentials)
+        .then(function(){
+          $location.path('blogList');
+        }
+        , (function(err){
+          vm.formError = "Error registering. Try again with a different email address."
+        }));
+    };
 
 
 }]);
